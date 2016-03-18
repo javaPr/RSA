dd0e6029c299374e2c537a915f12f5c0cd7b87fa
wdchang <13changice@tongji.edu.cn>
2016-03-18 20:53:52 +0800

    add Test.java by test branch

---
+++ src/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by wangdechang on 2016/3/18.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("only for testing");
+    }
+}
