dbc6f277d63eafef80421667f1d75f1f15404067
wdchang <13changice@tongji.edu.cn>
2016-03-18 21:12:33 +0800

    master

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -9,5 +9,8 @@ public class Test {
     public static void main(String[] args){
         System.out.println("only for testing");
         id=id+9;
+        int i = 0;
+        int j = i + 9;
+        
     }
 }
