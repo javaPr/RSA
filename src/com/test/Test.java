69c2db1b224e9cc87ac9bdb3e5ecfd4c6dc18633
wdchang <13changice@tongji.edu.cn>
2016-03-18 21:02:11 +0800

    test

---
+++ src/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Created by wangdechang on 2016/3/18.
+ */
+public class Test {
+    private static int id = 0;
+
+    public static void main(String[] args){
+        System.out.println("only for testing");
+        id=id+9;
+    }
+}
