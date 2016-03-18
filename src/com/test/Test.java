2ce536fc12b473b3845445d2adf617cefb7e5bf5
wdchang <13changice@tongji.edu.cn>
2016-03-18 20:58:00 +0800

    add id

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,10 @@ package com.test;
  * Created by wangdechang on 2016/3/18.
  */
 public class Test {
+    private static int id = 0;
+
     public static void main(String[] args){
         System.out.println("only for testing");
+        id=id+1;
     }
 }
